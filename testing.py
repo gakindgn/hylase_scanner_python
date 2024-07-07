f02a1a08ea298a9e4d56e7116d27838c5e32b43e
gakindgn <gunesakindogan@gmail.com>
2024-07-07 16:55:37 +0200

    finish

--- testing.py
+++ testing.py
@@ -1,21 +1,41 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
-import matplotlib.pyplot as plt
 
+import numpy as np # Import the NumPy library
+import random # Import the random library
+import os # Import the os library 
+from flask import send_from_directory  # Import the send_from_directory function from the Flask library
+
+
+# run matplotlib gui in main thread
+import matplotlib
+matplotlib.use('Agg') # Set the backend of Matplotlib to 'Agg' to save plots as images
+import matplotlib.pyplot as plt # Import the Matplotlib library
+
+
+
+
+# Create a Flask app
 app = Flask(__name__)
 
-# Function to collect data
+
+
+# function to load images from the static folder
+def load_file(filename):
+	return send_from_directory(filename)
+	
+
+
 def collect_data():
 	data = []
-	while True:
-		value = input("Enter a data point (or 'q' to quit): ")
-		if value == 'q':
-			break
-		data.append(float(value))
+	# generate random data
+	for i in range(100):
+		data.append(random.randint(0, 100))
 	return data
 
+
 # Function to visualize data
-def visualize_data(data):
+def visualize_data(data): 
 	plt.plot(data)
 	plt.xlabel('Data Point')
 	plt.ylabel('Value')
@@ -23,22 +43,18 @@ def visualize_data(data):
 	plt.savefig('static/plot.png')  # Save the plot as an image
 	plt.close()
 
+# route to load images
+@app.route('/<filename>')
+def send_image(filename):
+	return send_from_directory("static", filename)
+
+
 # Route to display the visualization in browser
-@app.route('/')
-def display_data():
+@app.route('/') 
+def index():
 	data = collect_data()
 	visualize_data(data)
 	return render_template('index.html', data=data)
-
-# Route to display specific amount of data
-@app.route('/plot', methods=['POST'])
-def display_specific_data():
-	start = int(request.form['start'])
-	end = int(request.form['end'])
-	data = collect_data()
-	selected_data = data[start:end]
-	visualize_data(selected_data)
-	return render_template('index.html', data=selected_data)
 
 # Main program
 if __name__ == '__main__':
