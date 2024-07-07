8f8be4576e35cf79508e56ea425b62c1880878c6
gakindgn <gunesakindogan@gmail.com>
2024-07-07 14:41:09 +0200

    local_server_data_display

--- testing.py
+++ testing.py
@@ -1,17 +1,45 @@
+from flask import Flask, render_template, request
 
-import socket
+import matplotlib.pyplot as plt
 
-def start_server():
-	server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	server_socket.bind(('localhost', 12345))
-	server_socket.listen()
-	print("Server is listening on localhost:12345")
-	
+app = Flask(__name__)
+
+# Function to collect data
+def collect_data():
+	data = []
 	while True:
-		client_socket, addr = server_socket.accept()
-		print(f"Connection from {addr} has been established.")
-		client_socket.send(bytes("Welcome to the server!", "utf-8"))
-		client_socket.close()
+		value = input("Enter a data point (or 'q' to quit): ")
+		if value == 'q':
+			break
+		data.append(float(value))
+	return data
 
-if __name__ == "__main__":
-	connect_to_server()
+# Function to visualize data
+def visualize_data(data):
+	plt.plot(data)
+	plt.xlabel('Data Point')
+	plt.ylabel('Value')
+	plt.title('Data Visualization')
+	plt.savefig('static/plot.png')  # Save the plot as an image
+	plt.close()
+
+# Route to display the visualization in browser
+@app.route('/')
+def display_data():
+	data = collect_data()
+	visualize_data(data)
+	return render_template('index.html', data=data)
+
+# Route to display specific amount of data
+@app.route('/plot', methods=['POST'])
+def display_specific_data():
+	start = int(request.form['start'])
+	end = int(request.form['end'])
+	data = collect_data()
+	selected_data = data[start:end]
+	visualize_data(selected_data)
+	return render_template('index.html', data=selected_data)
+
+# Main program
+if __name__ == '__main__':
+	app.run()
